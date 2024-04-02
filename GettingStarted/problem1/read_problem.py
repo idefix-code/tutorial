f74421a4daece2fe1716997658059bddd319ed69
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2024-04-02 17:33:24 +0200

    fix read_probleM

--- GettingStarted/problem1/read_problem.py
+++ GettingStarted/problem1/read_problem.py
@@ -11,19 +11,21 @@ n = int(sys.argv[1])
 V=readVTK("data.%04d.vtk"%n)
 
 for field in V.data.keys():
-  plt.figure()
+  plt.figure(figsize=(10,4))
   plt.pcolormesh(V.x,V.y,V.data[field][:,:,0].T)
   plt.title(field+ " @ t=%f"%V.t)
   plt.colorbar()
+  plt.gca().set_aspect('equal')
   
 # compute vorticity
 wz = np.gradient(V.data['VX2'],V.x,axis=0)-np.gradient(V.data['VX1'],V.y,axis=1)
 
-plt.figure()
+plt.figure(figsize=(10,4))
 plt.pcolormesh(V.x,V.y,wz[:,:,0].T)
 plt.title(r"$\omega_z$ @ t=%f"%V.t)
 plt.colorbar()
-  
+plt.gca().set_aspect('equal')
+
 plt.show()
 
 
